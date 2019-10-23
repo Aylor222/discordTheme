843efd1064813019ea37aa52d295fd34222de75d
Aylor <aylor@hotmail.fr>
2019-10-23 07:28:29 -0400

    Increase image compatibilities

--- RandomBackground_plugin_core.js
+++ RandomBackground_plugin_core.js
@@ -2,11 +2,21 @@
 class customBackground {
 
     constructor(){
+        this.fs = require('fs');
+
         this.availableImages = [];
         this.cooldown = 300000;
         this.clock;
-        this.fs = require('fs');
         this.initialized = false;
+
+        this.filesFormatAuthorized = [
+            ".jpg",
+            ".jpeg",
+            ".bmp",
+            ".tif",
+            ".gif",
+            ".png"
+        ];
     }
 
     addButtonChangeBackground() {
@@ -14,12 +24,8 @@ class customBackground {
     		return;
        	var parent = $(".typeWindows-1za-n7");
     	var className = "customBackground_changeBg winButtonMinMax-PBQ2gm winButton-iRh8-Z flexCenter-3_1bcw flex-1O1GKY justifyCenter-3D2jYp alignCenter-1dQNNs da-winButtonMinMax da-winButton da-flexCenter da-flex da-justifyCenter da-alignCenter";
-		var element = $('<div id="customBackground_changeBgBtn" class="' + className + '" onclick="bdplugins.CustomBackground.plugin.changeBackground()"></div>');
+		var element = $(`<div id="customBackground_changeBgBtn" class="${className}" onclick="bdplugins.CustomBackground.plugin.changeBackground()"></div>`);
     	parent.append(element);
-    }
-
-    changebackground() {
-    	this.changeBackground();
     }
 
     changeBackground() {
@@ -30,13 +36,13 @@ class customBackground {
         }, this.cooldown);
         this.fs.readFile(this.availableImages[Math.floor(Math.random()*this.availableImages.length)], function(err, data) {
             var base64data = new Buffer(data).toString('base64');
-            $("#app-mount").css("background-image","url(data:image/png;base64," + base64data + ")");
+            $("#app-mount").css("background-image",`url(data:image/png;base64,${base64data})`);
         });
     }
 
     getName() {return "CustomBackground";} 
     getDescription() {return "Random Background Image";} 
-    getVersion() {return "1.0.0";} 
+    getVersion() {return "1.0.1";} 
     getAuthor() {return "Aylor";} 
 
     load() {
@@ -46,7 +52,7 @@ class customBackground {
         var that = this;
         var directory = __filename;
 
-        directory = directory.substring(0, directory.indexOf("\\plugins")) + "\\images\\";
+        directory = `${directory.substring(0, directory.indexOf("\\plugins"))}\\images\\`;
 
         // Create folder
         this.fs.access(directory, this.fs.constants.R_OK, (err) => {
@@ -57,7 +63,7 @@ class customBackground {
                     }).catch(function reject() {});
                 } catch (err) {
                     if (err.code !== 'EEXIST') {
-                        console.log("Code Error: " + err.code);
+                        console.log(`Code Error: ${err.code}`);
                     }
                 }
             } else {
@@ -68,16 +74,14 @@ class customBackground {
 
     loadImages(directory) {
         this.fs.readdir(directory, (err, files) => {
-        	//if more images has been added since last clock cycle -> add to cache
-        	if(this.availableImages.length == files.length)
-        		return;
-
         	this.availableImages = [];
             files.forEach(file => {
-                // jpg, png
-                if(file.toString().endsWith(".jpg") || file.toString().endsWith(".png"))
+
+                var regex = (new RegExp(`${this.filesFormatAuthorized.join('|')}$`, 'g'));
+                if(regex.test(file.toString()))
                     this.availableImages.push(directory + file);
             });
+            console.log(this.availableImages);
             // Clock change background every x minutes
             this.changeBackground();
         });
@@ -91,7 +95,7 @@ class customBackground {
             this.loadDirectory();
         }
         var that = this;
-        setTimeout(function() {
+        setTimeout(function() { // Timeout to let the page load.
         	that.addButtonChangeBackground();
         }, 2000);
     } 
